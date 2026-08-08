5f07f038757035b2933f0e20c049f525544dd621
Gene Lee <gene.clee6@gmail.com>
2026-08-08 15:37:13 +0800

    index setup and db connected

---
+++ src/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const helmet = require("helmet");
+const express = require("express");
+const cors = require("cors");
+const logger = require("./utils/logger");
+const morganMiddleware = require("./middleware/morgan.middleware");
+const rateLimiter = require("./middleware/rateLimit.middleware");
+const connectDB = require("./utils/db");
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+app.use(helmet());
+app.use(morganMiddleware);
+app.use(rateLimiter);
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+app.listen(PORT, () => {
+    logger.info(`Server listening on port ${PORT}`);
+});
---
+++ src/utils/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const logger = require("./logger");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        logger.info("DB connected");
+    } catch (e) {
+        logger.error(`DB connection failed, ${e.message}`);
+        // 0
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
